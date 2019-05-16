165d2fa48b50a94c4544972fc442ecfeed5b62fc
HongxiChen <chenh640@newschool.edu>
2019-05-16 13:05:51 -0400

    final

---
+++ VRTest/Assets/Script/NegativeDictionary.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NegativeDictionary : MonoBehaviour
+{
+    Dictionary<string, int> nDictionary;
+    int score = 0;
+
+    void Start()
+    {
+        nDictionary = new Dictionary<string, int>();
+
+        nDictionary.Add("hate", -10);
+        nDictionary.Add("dislike",-10);
+        nDictionary.Add("awful", -10);
+        nDictionary.Add("hopeless", -10);
+        nDictionary.Add("give up", -10);
+        nDictionary.Add("disappoited", -10);
+        nDictionary.Add("cry", -10);
+        nDictionary.Add("boring", -10);
+        nDictionary.Add("what is the purpose", -10);
+        nDictionary.Add("angry", -10);
+        nDictionary.Add("confused", -10);
+        nDictionary.Add("lonely", -10);
+        nDictionary.Add("depressed", -10);
+        nDictionary.Add("shame", -10);
+        nDictionary.Add("scary", -10);
+        nDictionary.Add("bad", -10);
+
+       foreach(KeyValuePair<string,int> pair in nDictionary) {
+            Debug.Log(pair.Key + "" + pair.Value);
+       }
+
+
+        bool isExist = nDictionary.ContainsKey("happy");
+        print("if exist happy" + isExist);
+
+        if (isExist == true)
+        {
+            score = score - 10;
+        }
+
+
+        Debug.Log("this is Score" + score);
+    }
+}
---
+++ VRTest/Assets/Script/PositiveDictionary.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositiveDictionary : MonoBehaviour
+{
+
+    Dictionary<string, int> pDictionary;
+    int score = 0;
+
+    void Start()
+    {
+        pDictionary = new Dictionary<string, int>();
+        pDictionary.Add("happy", 10);
+        pDictionary.Add("like", 10);
+        pDictionary.Add("meaningful", 10);
+        pDictionary.Add("helpful", 10);
+        pDictionary.Add("kind", 10);
+        pDictionary.Add("friendly", 10);
+        pDictionary.Add("cool", 10);
+        pDictionary.Add("fun", 10);
+        pDictionary.Add("funny", 10);
+        pDictionary.Add("awesome", 10);
+        pDictionary.Add("interesting", 10);
+        pDictionary.Add("beautiful", 10);
+        pDictionary.Add("wonderful", 10);
+        pDictionary.Add("fantastic", 10);
+        pDictionary.Add("incredible", 10);
+        pDictionary.Add("upbeat", 10);
+        pDictionary.Add("peaceful", 10);
+        pDictionary.Add("satisfied", 10);
+        pDictionary.Add("happiness", 10);
+        pDictionary.Add("love", 10);
+        pDictionary.Add("successful", 10);
+        pDictionary.Add("excited", 10);
+
+
+        string words = "happy";
+
+
+        bool isExist = pDictionary.ContainsKey(words);
+        print("if exist happy" + isExist);
+
+        //isExist = pDictionary.ContainsKey ("hate");
+        //print("if exist hate: " + isExist);
+
+
+
+
+        if (isExist == true) {
+            score = score + 10;
+        }
+        else {
+            score = score + 0;
+        }
+
+        Debug.Log("this is Score" + score);
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
